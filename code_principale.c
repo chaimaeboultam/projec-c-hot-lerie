46fb8a1c884f104b76010d79e348f95d451ead1b
Mohammed janati <Mohammedjanati0303@gmail.com>
2023-11-05 23:28:00 +0100

    client

--- code_principale.c
+++ code_principale.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 int main()
-{   int rep0,rep1;
+{   int rep0,rep1,r;
+    client f;
+      user_choice:
     printf("===============================================\n");
     printf("      WELCOME  to Hotel TRANSYLVANIA\n");
     printf("===============================================\n");
@@ -22,42 +25,57 @@ int main()
             do{
             printf("HI dear client, Welcome to our hotel!\n");
             printf("\n");
+            menu:
             printf("===============MENU=================\n");
             printf("\n");
             printf("1-Discover our rooms\n");
             printf("\n");
             printf("2-Book a stay\n");
             printf("\n");
-            printf("3-Discover our services\n");
+            printf("3-Discover our services and promotion\n");
             printf("\n");
             printf("4-Give us your feedback\n");
             printf("\n");
-            printf("5-you need help?\n");
+            printf("5-Quit\n");
             printf("\n");
             printf("Please enter the number corresponding to the desired option:");
             scanf("%d",&rep1);
 
             if(rep1<1 || rep1>5){
                 printf("\n");
-                printf("inexisted number please try to entre an available numbre\n\n");} 
+                printf("inexisted number please try to entre an available numbre\n\n");}
             }while(rep1<1 || rep1>5);
 
             switch(rep1){
-                case 1:
-                   // discoverroom();
-                    break;
-                case 2:
-                    //reserve();
-                    break;
-                case 3:
-                   // service();
-                    break;
-                case 4:
-                   // feedback();
-                    break;
-                case 5:
-                   // help();
-                    break;
+                case 1:{
+                    //****************************clear console
+                   discoverroom();
+                   printf("you want to return to menu (0:no/1:yes)");
+                   scanf("%d",&r);
+                   if(r==1)
+                   goto menu;
+                    break;}
+                case 2:{
+                    //reserve()
+                    break;}
+                case 3:{
+                    // *****************************clear screen
+                    service();
+                    printf("you want to return to menu (0:no/1:yes)");
+                   scanf("%d",&r);
+                   if(r==1)
+                   goto menu;
+                    break;}
+                case 4:{
+                   feedback(&f);
+                    printf("you want to return to menu (0:no/1:yes)");
+                   scanf("%d",&r);
+                   if(r==1)
+                   goto menu;
+                    break;}
+                case 5:{
+                    goto user_choice;
+                    break;}
 
                 }
 
--- main.h
+++ main.h
@@ -18,7 +18,7 @@ typedef struct room{
 typedef struct client{
     int Cin;
     int phone;
-    char name[30];
+    char name[30],feedback[100];
     char adresse[30];
     int reservationDetails[MAXResrvation];
 }client;
@@ -29,6 +29,79 @@ typedef struct reservation{
     char checkIn[10];
     char checkOut[10];
 }reservation;
+
+
+
+//***********************declaration des fonction************************
+void discoverroom();
+void service();
+void feedback(client *f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  ********************************definition des fonctions******************************************
+
+void discoverroom(){
+ printf("in our hotel we have the following type of rooms :\n\n"
+"          NORMAL ROOM :(price of 150$ per night)\n\n"
+" you will get:-Cozy and spacious with a choice of single, double, or twin beds.\n"
+"              -Bathroom with a hot shower and complimentary toiletries.\n"
+"              -Flat-screen TV with a wide selection of cable channels.\n"
+"              -High-speed Wi-Fi for staying connected.\n"
+"              -Mini refrigerator to keep your beverages and snacks cool.\n"
+"              -In-room coffee and tea-making facilities.\n"
+"              -Work desk for business travelers.\n"
+"              -Complimentary daily housekeeping to keep your room fresh and tidy.\n");
+
+printf("\n*************************************************************************************\n\n");
+
+printf("   NORMAL ROOM :(price of 1500$ per night)\n\n"
+" you will get:-Exquisite, upscale furnishings and decor.\n"
+"              -A spacious layout with king-sized beds for maximum comfort.\n"
+"              -Lavish en-suite bathrooms with a bathtub and premium toiletries.\n"
+"              -A private balcony or terrace with stunning views of the surroundings.\n"
+"              -State-of-the-art entertainment system with a large flat-screen TV.\n"
+"              -High-speed Wi-Fi for seamless connectivity.\n"
+"              -A fully stocked mini-bar with a selection of snacks and beverages.\n"
+"              -In-room espresso machine for your coffee indulgence.\n"
+"              -Personalized concierge service to cater to your needs.\n"
+"              -Complimentary access to our exclusive lounge or spa facilities.\n");
+}
+
+void service(){
+    printf("our additional service and promotion:\n\n"
+    "        ~spa(20$)\n     "
+    "   ~gym(20$)\n     "
+    "   ~for special client who reserve in our hotel in last 30 day will have reduction of 50%% from the price of the room \n   "
+    );
+}
+ void feedback(client *f){
+
+    printf("we are glad to hear your feedback:\n");
+    scanf(" %[^\n]",f->feedback);
+    printf("thank for your feedback\n");
+ }
+
+
 
 
 #endif // MAIN_H
